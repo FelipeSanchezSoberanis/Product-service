209273d493025329e1d5e74ee45274fb0d121911
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2022-08-04 09:33:57 -0500

    Initial commit

---
+++ src/test/java/com/exampleApp/productService/ProductServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.exampleApp.productService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
